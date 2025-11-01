3a5a3834d2d8bbb5963f43ffb1421230a2342e1c
Oceanicshadow88 <oceanicshadow88@gmail.com>
2025-11-01 21:52:12 +1100

    update todayWeatherCard to function comp

--- src/components/TodayWeatherCard/TodayWeatherCard.js
+++ src/components/TodayWeatherCard/TodayWeatherCard.js
@@ -1,62 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodayWeatherCard.css';
 import DynamicWeather from '../DynamicWeather/DynamicWeather';
 
-class TodayWeatherCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            elementHeight: 0,
-            elementWidth: 0,
-        };
-    }
+const TodayWeatherCard = (props) => {
+    const [elementHeight, setElementHeight] = useState(0);
+    const [elementWidth, setElementWidth] = useState(0);
 
-    getHeight = (element) => {
-        const { elementHeight } = this.state;
+    const getHeight = (element) => {
         if (element && !elementHeight) {
             // need to check that we haven't already set the height or we'll create an infinite render loop
-            this.setState({ elementHeight: element.clientHeight, elementWidth: element.clientWidth });
+            setElementHeight(element.clientHeight);
+            setElementWidth(element.clientWidth);
         }
     };
 
-    render() {
-        const { data } = this.props;
-        const { elementWidth, elementHeight } = this.state;
-        return (
-            <section className="card__current" ref={this.getHeight}>
-                <DynamicWeather
-                    data={data}
-                    height={parseInt(elementHeight, 10)}
-                    width={parseInt(elementWidth, 10)}
-                    timeOverride={this.props.timeOverride}
-                />
-                <div className="card__current-temperature">
-                    <div className="center">
-                        <span>
-                            {parseInt(data.currently.temperature, 10)}°
-                        </span>
-                    </div>
-                    <div className="center">
-                        <p className="clear-margin clear-padding card_current_summary">{data.currently.summary}</p>
-                    </div>
-                    <ul className="card__current-details-list flex space-between list-style--disable center">
-                        <li className="card__current-details">
-                            <p>HUMIDITY</p>
-                            <p>{parseInt(data.currently.humidity * 100, 10)}%</p>
-                        </li>
-                        <li className="card__current-details">
-                            <p>WIND</p>
-                            <p>{data.currently.windSpeed} m/s</p>
-                        </li>
-                    </ul>
+    const { data, timeOverride } = props;
+
+    return (
+        <section className="card__current" ref={getHeight}>
+            <DynamicWeather
+                data={data}
+                height={parseInt(elementHeight, 10)}
+                width={parseInt(elementWidth, 10)}
+                timeOverride={timeOverride}
+            />
+            <div className="card__current-temperature">
+                <div className="center">
+                    <span>
+                        {parseInt(data.currently.temperature, 10)}°
+                    </span>
                 </div>
-                <div className="card__current-location">
-                    <p className="card__current-country">{data.timezone.split('/')[1].replace('_', ' ')}</p>
+                <div className="center">
+                    <p className="clear-margin clear-padding card_current_summary">{data.currently.summary}</p>
                 </div>
-            </section>
-        );
-    }
-}
+                <ul className="card__current-details-list flex space-between list-style--disable center">
+                    <li className="card__current-details">
+                        <p>HUMIDITY</p>
+                        <p>{parseInt(data.currently.humidity * 100, 10)}%</p>
+                    </li>
+                    <li className="card__current-details">
+                        <p>WIND</p>
+                        <p>{data.currently.windSpeed} m/s</p>
+                    </li>
+                </ul>
+            </div>
+            <div className="card__current-location">
+                <p className="card__current-country">{data.timezone.split('/')[1].replace('_', ' ')}</p>
+            </div>
+        </section>
+    );
+};
 
 export default TodayWeatherCard;
 
